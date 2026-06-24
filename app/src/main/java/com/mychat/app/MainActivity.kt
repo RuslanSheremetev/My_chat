213b390cf2f4b7dd61e5c232e63389265d97fddb
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-06-25 01:58:43 +0200

    Add missing IOException import

--- app/src/main/java/com/mychat/app/MainActivity.kt
+++ app/src/main/java/com/mychat/app/MainActivity.kt
@@ -37,6 +37,7 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.File
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
