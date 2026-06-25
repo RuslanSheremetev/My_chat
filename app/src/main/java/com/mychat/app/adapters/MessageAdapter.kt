1f5e972a2187777dc99b6e50991a7b0842b8e27e
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-06-25 10:55:17 +0200

    Fix duplicate imports and readStatus

--- app/src/main/java/com/mychat/app/adapters/MessageAdapter.kt
+++ app/src/main/java/com/mychat/app/adapters/MessageAdapter.kt
@@ -2,7 +2,6 @@ package com.mychat.app.adapters
 
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.ImageView
