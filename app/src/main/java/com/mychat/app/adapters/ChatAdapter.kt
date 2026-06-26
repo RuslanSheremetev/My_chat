25cb9b64626279eb4558d93fa36219a47b44462f
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-06-26 04:48:55 +0200

    Add ImageView import to ChatAdapter

--- app/src/main/java/com/mychat/app/adapters/ChatAdapter.kt
+++ app/src/main/java/com/mychat/app/adapters/ChatAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.mychat.app.R
 import com.mychat.app.models.User
