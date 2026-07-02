40b1113c3e6553c686f63263ab8a2bf073087e96
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-07-02 17:26:52 +0200

    Fix: add ImageButton import to MapsActivity

--- app/src/main/java/com/mychat/app/activities/MapsActivity.kt
+++ app/src/main/java/com/mychat/app/activities/MapsActivity.kt
@@ -3,6 +3,7 @@ package com.mychat.app.activities
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ImageButton
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
