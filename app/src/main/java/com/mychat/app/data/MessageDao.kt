630111b531c766d81d04b2417741f6cef34912d0
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-07-09 23:23:07 +0200

    Fix: persist read status to Room on receipt

--- app/src/main/java/com/mychat/app/data/MessageDao.kt
+++ app/src/main/java/com/mychat/app/data/MessageDao.kt
@@ -54,3 +54,6 @@ interface MessageDao {
     @Query("UPDATE chat_settings SET unread = 0 WHERE chatKey = :chatKey")
     fun resetUnread(chatKey: String)
 }
+
+    @Query("UPDATE messages SET read = 1 WHERE chatKey = :chatKey AND fromUser = :fromUser AND read = 0")
+    fun markMessagesRead(chatKey: String, fromUser: String)
