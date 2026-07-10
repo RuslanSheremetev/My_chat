c326c8bc73eef09059b864daad067a5aa6dc5146
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-07-10 08:47:54 +0200

    Fix: change delivered to var in ChatMessage

--- app/src/main/java/com/mychat/app/models/ChatMessage.kt
+++ app/src/main/java/com/mychat/app/models/ChatMessage.kt
@@ -4,7 +4,7 @@ data class ForwardData(val from: String = "", val text: String = "", val file: F
 data class ChatMessage(
     val id: String, val from: String, val to: String, val text: String = "", val time: String = "",
     val file: FileInfo? = null, val location: LocationData? = null, val isGroup: Boolean = false,
-    var read: Boolean = false, val delivered: Boolean = false,
+    var read: Boolean = false, var delivered: Boolean = false,
     val reactions: MutableMap<String, MutableList<String>> = mutableMapOf(),
     val forward: ForwardData? = null
 )
