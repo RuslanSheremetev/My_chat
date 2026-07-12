62008df1d73dd74f3d40fab290aeb00ffdc0889c
RuslanSheremetev <ruslanpogrebnyak90@gmail.com>
2026-07-12 23:49:15 +0200

    Fix: импорт kotlin.concurrent.thread

--- app/src/main/java/com/mychat/app/activities/SettingsActivity.kt
+++ app/src/main/java/com/mychat/app/activities/SettingsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.*
 import android.app.AlertDialog
+import kotlin.concurrent.thread
 import androidx.appcompat.app.AppCompatActivity
 import com.mychat.app.R
 
